a4a00aa02932d5beed698e3ffb1d93d2a36e94a8
Akbarshah <akbarshah19@icloud.com>
2023-05-10 18:52:19 +0500

    Initial Commit

---
+++ ToDoList_SwiftUI/ToDoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUIApp.swift
+//  ToDoList_SwiftUI
+//
+//  Created by Akbarshah Jumanazarov on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
